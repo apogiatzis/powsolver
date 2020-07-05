28054735d954607b86b9dd43ce82503aaa6f6fdd
apogiatzis <apoyiatzis@outlook.com>
2020-07-05 05:34:03 +0100

    chore: update package deps

--- setup.py
+++ setup.py
@@ -2,18 +2,30 @@ from setuptools import setup
 
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='powsolver',
-      version='0.1.2',
-      description='Proof of Work solver mainly for CTF challenges',
-      url='http://github.com/apogiatzis/powsolver',
-      author='Antreas Pogiatzis',
-      author_email='pogiatzis.c.a@gmail.com',
-      license='MIT',
-      packages=['powsolver'],
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      zip_safe=False)
+setup(
+    install_requires=[
+        "dill==0.3.2",
+        "multiprocess==0.70.10",
+        "parse==1.15.0",
+        "pathos==0.2.6",
+        "pox==0.2.8",
+        "ppft==1.6.6.2",
+        "six==1.15.0",
+    ],
+    name="powsolver",
+    version="0.1.2",
+    description="Proof of Work solver mainly for CTF challenges",
+    url="http://github.com/apogiatzis/powsolver",
+    author="Antreas Pogiatzis",
+    author_email="pogiatzis.c.a@gmail.com",
+    license="MIT",
+    packages=["powsolver"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+)
