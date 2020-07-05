ac6d578c1a7f74dbe4a42b1afebfd40a26ac67dd
apogiatzis <apoyiatzis@outlook.com>
2020-07-05 22:03:42 +0100

    chore: setting up pypi package pub pipeline

--- setup.py
+++ setup.py
@@ -28,4 +28,17 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography'
+    ],
 )
